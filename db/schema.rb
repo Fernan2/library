39f64dbf5af8b520a26fef2706e653b3099a1ca7
Fernando Calatayud <fernandorankia@gmail.com>
2016-11-26 10:17:29 +0100

    Create new model Review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126090229) do
+ActiveRecord::Schema.define(version: 20161126091631) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20161126090229) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "reviewable_type"
+    t.integer  "reviewable_id"
+    t.string   "text"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
