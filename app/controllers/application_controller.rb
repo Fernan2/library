12c075f2677742ab9444cd792d56c23aba98dfe7
Fernando Calatayud <fernandorankia@gmail.com>
2016-11-26 13:25:17 +0100

    add variants by parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_variant
+
+  def set_variant
+    return unless params[:variant].in?(%w(phone tablet desktop))
+    request.variant = params[:variant].to_sym
+  end
 end
