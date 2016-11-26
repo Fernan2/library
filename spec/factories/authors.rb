924fa074f4bfd6c3e5326f5e6956b91cb772c797
Fernando Calatayud <fernandorankia@gmail.com>
2016-11-26 12:40:37 +0100

    Use traits & sub-factories on Author

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -2,10 +2,18 @@ FactoryGirl.define do
   factory :author do
     first_name { FFaker::Name.first_name }
     last_name { FFaker::Name.last_name }
-    email { FFaker::Internet.email }
-    phone "MyString"
-    city "MyString"
-    address "MyString"
-    website "MyString"
+    email { "#{first_name}@#{last_name}.com".downcase }
+
+    trait :complete_address do
+      city "MyString"
+      address "MyString"
+    end
+
+    trait :contact_data do
+      phone "MyString"
+      website "MyString"
+    end
+
+    factory :author_full_data, traits: [:complete_address, :contact_data]
   end
 end
