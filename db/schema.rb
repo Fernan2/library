c1f9e6e4c1ceccab9731249b51ab59068cf14cf4
Fernando Calatayud <fernandorankia@gmail.com>
2016-11-26 10:13:29 +0100

    Add counter_cache books_count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109220324) do
+ActiveRecord::Schema.define(version: 20161126090229) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20161109220324) do
     t.string   "city"
     t.string   "address"
     t.string   "website"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "books_count", default: 0
   end
 
   create_table "books", force: :cascade do |t|
