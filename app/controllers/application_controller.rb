9060f9cd94e9846190603564cea53ef365ffe880
Fernando Calatayud <fernandorankia@gmail.com>
2016-12-07 09:57:41 +0100

    Add locale configuration & es_ES, en_US

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_variant
+  before_action :set_locale
 
   def set_variant
     request.variant = :phone if browser.device.mobile?
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale]
   end
 end
