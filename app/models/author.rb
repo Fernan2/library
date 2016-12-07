fb51aae707e5afa4cc5944e4c0bb9cf4cab05156
Fernando Calatayud <fernandorankia@gmail.com>
2016-12-07 17:28:21 +0100

    Clean author.first_name/last_name

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,9 +3,18 @@ class Author < ApplicationRecord
   has_many :books
   validates :email, :first_name, presence: true
 
+  before_save :clean_name
+
   REDIRECTED_AUTHORS = { '124' => 7, '25' => 2 }
 
   def to_s
-    "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}".strip
+  end
+
+  private
+
+  def clean_name
+    self.first_name = first_name.titleize
+    self.last_name = last_name.to_s.titleize
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Author, type: :model do
   end
 
   it { is_expected.to validate_presence_of(:first_name) }
+
+  context 'with an all-upcase last_name and all-lowercase first_name' do
+    let(:author) { FactoryGirl.build(:author, first_name: 'john', last_name: 'SMITH') }
+
+    it 'should be titleized after save' do
+      author.save
+      expect(author.reload.to_s).to eq('John Smith')
+    end
+  end
+
+  context 'with an all-upcase last_name and all-lowercase first_name' do
+    let(:author) { FactoryGirl.build(:author, first_name: 'john', last_name: nil) }
+
+    it 'should be titleized after save' do
+      author.save
+      expect(author.reload.to_s).to eq('John')
+    end
+  end
 end
