e24c831085d1ebe7221d21d25eba25092aaf3b81
Fernando Calatayud <fernandorankia@gmail.com>
2016-12-07 12:07:37 +0100

    Handle 404 errors on ApplicationController
    Should be improved with a search box, suggestions and so

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_variant
   before_action :set_locale
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  private
+
+  def render_404
+    render 'web/404'
+  end
 
   def set_variant
     request.variant = :phone if browser.device.mobile?
